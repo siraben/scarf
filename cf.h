e0c9af67e2ceafb4c5b9f84b2d89891b40f4a906
izder456 <izder456@protonmail.com>
2020-10-04 02:20:39 -0500

    add Obj-C version and fix an error in main.c

--- cf.h
+++
@@ -1,8 +0,0 @@
-///////////////////////////////////////
-// Name     : C-Flat Header
-// Author   : izder456
-// License  : Simplified 2-Clause BSD
-// Version  : a0.1
-///////////////////////////////////////
-
-
